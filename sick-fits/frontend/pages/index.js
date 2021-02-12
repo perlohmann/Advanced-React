a811a8be1306b058fd15c77718fc1e7969aa2ffb
Per Lohmann <43436115+perlohmann@users.noreply.github.com>
2021-02-12 08:37:03 +0100

    initial commit
    Initial commit to the fork

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Page from "../components/Page";
+
+export default function IndexPage() {
+  return (
+    <Page cool="Heck ya!">
+      <p>Hello!</p>
+      <code>var</code>
+    </Page>
+  );
+}
